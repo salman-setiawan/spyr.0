8b9f48e2c30d021dd59f2e794a3cdc0c734b8f09
Salman Setiawan <salmansetiawan88@gmail.com>
2024-07-04 20:10:25 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from threading import Thread
+import discord
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+# Panggil fungsi keep_alive sebelum menjalankan bot
+keep_alive()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+# Token bot Anda di sini
+token = os.getenv("DISCORD_BOT_TOKEN")
+client.run(token)
